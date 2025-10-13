ddb518d82dfea57bf101a4d5e22bb655b9b6ccbf
PepeTureatca <aletur4@alu.edu.gva.es>
2025-10-13 16:28:49 +0200

    Creacion clase CintaVideo

---
+++ archivos/CintaVideo.php
@@ -0,0 +1,17 @@
+<?php
+class CintaVideo extends Soporte
+{
+    private $duracion;
+
+    public function __construct($titulo, $numero, $precio, $duracion)
+    {
+        parent::__construct($titulo, $numero, $precio);
+
+        $this->duracion = $duracion;
+    }
+    public function __muestraResumen($numero)
+		{
+            parent::__muestraResumen();
+		}
+	}
+
