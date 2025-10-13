89e0e1dd06f6b6d6b1a82307cac30fe0ae7618d9
Sebas <juaagucas@alu.edu.gva.es>
2025-10-13 16:36:51 +0200

    metodo mostrar resumen

--- archivos/Soporte.php
+++ archivos/Soporte.php
@@ -12,9 +12,24 @@
 			$this->precio = $precio;
 		}
 
-		public function __getPrecio($numero)
+		public function __getPrecio($precio)
 		{
 			return $this->precio;
+		}
+
+		public function __getPrecioConIva($precio)
+		{
+			return $this->precio * 1.21;
+		}
+
+		public function __getNumero($numero)
+		{
+			return $this->precio;
+		}
+
+		public function __muestraResumen()
+		{
+			return  ;
 		}
 	}
 ?>
