2e6da98578e79f551835d2e87f12fcbde46b760c
Pepe <aletur4@alu.edu.gva.es>
2025-10-14 18:34:52 +0200

    Ejercicio 5

---
+++ archivos/Client.php
@@ -0,0 +1,16 @@
+<?php
+class Cliente
+{
+public $nombre;
+private $numero;
+private $soportesAlquilados = [];
+private $numSoportesAlquilados;
+private $maxAlquilerConcurrente;
+public function __construct()
+    {
+        parent::__construct($titulo, $numero, $precio);
+
+        $this->idiomas = $idiomas;
+        $this->formatoPantalla = $formatoPantalla;
+    }
+}
