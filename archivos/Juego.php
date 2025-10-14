487668ee187062b8b67c864fa8445ac6963551fa
sebas <juaagucas@alu.edu.gva.es>
2025-10-14 18:27:46 +0200

    Ejercicio 4

---
+++ archivos/Juego.php
@@ -0,0 +1,33 @@
+<?php
+class Juego extends Soporte
+{
+    public $consola;
+    private $minNumJugadores = 0;
+    private $maxNumJugadores = 0;
+
+    public function __construct($titulo, $numero, $precio, $consola, $minNumJugadores, $maxNumJugadores)
+    {
+        parent::__construct($titulo, $numero, $precio);
+
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;
+    }
+
+    public function muestraJugadoresPosibles()
+    {
+        if ($this->minNumJugadores == 1 && $this->maxNumJugadores == 1) 
+        {
+            echo "<br> Para un jugador";
+        }else
+        {
+            echo "<br> Para varios jugadores";
+        }
+    }
+
+    public function muestraResumen()
+    {
+        parent::muestraResumen();
+        echo $this->muestraJugadoresPosibles();
+    }
+}
--- archivos/inicio.php
+++ archivos/inicio.php
@@ -37,3 +37,15 @@ echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
 echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
 $miDvd->muestraResumen();
  ?>
+
+ <?php
+echo "<h2>EJERCICIO 4 (323)</h2> ";
+
+include "Juego.php";
+
+$miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1); 
+echo "<strong>" . $miJuego->titulo . "</strong>"; 
+echo "<br>Precio: " . $miJuego->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $miJuego->getPrecioConIva() . " euros";
+$miJuego->muestraResumen();
+ ?>
