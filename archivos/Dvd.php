6f8117605c953e10ace7ec6c6b61c92178d4028a
Pepe <aletur4@alu.edu.gva.es>
2025-10-14 18:13:15 +0200

    Ejercicio 3 DVD

---
+++ archivos/Dvd.php
@@ -0,0 +1,20 @@
+<?php
+class Dvd extends Soporte
+{
+    public $idiomas;
+    private $formatoPantalla;
+
+    public function __construct($titulo, $numero, $precio, $idiomas, $formatoPantalla)
+    {
+        parent::__construct($titulo, $numero, $precio);
+
+        $this->idiomas = $idiomas;
+        $this->formatoPantalla = $formatoPantalla;
+    }
+    public function muestraResumen()
+    {
+        parent::muestraResumen();
+        echo "<br> Idiomas:" . $this->idiomas;
+        echo "<br> Formato Pantalla:" . $this->formatoPantalla;
+    }
+}
--- archivos/inicio.php
+++ archivos/inicio.php
@@ -3,17 +3,37 @@ include "Soporte.php";
 
 echo "<h1>VIDEOCLUB</h1>";
 echo "<h2>EJERCICIO 1 (320)</h2> ";
-$soporte1 = new Soporte("Tenet", 22, 3); 
-echo "<strong>" . $soporte1->titulo . "</strong>"; 
-echo "<br>Precio: " . $soporte1->getPrecio() . " euros"; 
+
+$soporte1 = new Soporte("Tenet", 22, 3);
+echo "<strong>" . $soporte1->titulo . "</strong>";
+echo "<br>Precio: " . $soporte1->getPrecio() . " euros";
 echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros";
 $soporte1->muestraResumen();
+?>
 
+<?php
+include "CintaVideo.php";
 
 echo "<h2>EJERCICIO 2 (321)</h2> ";
-include "CintaVideo.php";
-$miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107); 
-echo "<strong>" . $miCinta->titulo . "</strong>"; 
-echo "<br>Precio: " . $miCinta->getPrecio() . " euros"; 
+
+$miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+echo "<strong>" . $miCinta->titulo . "</strong>";
+echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
 echo "<br>Precio IVA incluido: " . $miCinta->getPrecioConIva() . " euros";
 $miCinta->muestraResumen();
+?>
+
+
+
+
+<?php
+echo "<h2>EJERCICIO 3 (322)</h2> ";
+
+include "Dvd.php";
+
+$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+echo "<strong>" . $miDvd->titulo . "</strong>";
+echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
+echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
+$miDvd->muestraResumen();
+ ?>
