5cf1130543c731f177e99e13c0dd682391e39f16
Pepe <aletur4@alu.edu.gva.es>
2025-10-20 16:56:25 +0200

    actualizacion clase Videoclub socios

--- archivosVideoclub1.0/Videoclub.php
+++ archivosVideoclub1.0/Videoclub.php
@@ -1,4 +1,14 @@
 <?php
+spl_autoload_register(function ($class) {
+    $archivo = __DIR__ . '/' . $class . '.php';
+    if (file_exists($archivo)) {
+        require_once $archivo;
+    } else {
+        echo "No se encontró la clase $class en $archivo<br>";
+    }
+});
+
+
 class Videoclub
 {
 
@@ -7,59 +17,51 @@ class Videoclub
     private $numProductos;
     private $socios = [];
     private $numSocios;
- 
-<<<<<<< HEAD
-    private function incluirProducto()
+
+    public function __construct($nombre)
     {
-
+        $this->nombre = $nombre;
     }
+    private function incluirProducto() {}
 
-    public function incluirCintaVideo($titulo, $precio, $duracion)
+    public function incluirCintaVideo($titulo, $precio, $duracion) {}
+
+    public function Dvd($titulo, $precio, $idiomas, $pantalla) {}
+
+    public function incluirJuego($titulo, $precio, $consola, $minJ, $maxJ) {}
+
+    public function incluirSocio($nombre, $maxAlquileresConcurrentes = 3)
     {
-
+        $numero = count($this->socios) + 1;
+        $nuevoCliente = new Cliente($nombre, $numero, $maxAlquileresConcurrentes);
+        $this->socios[] = $nuevoCliente;
+        $this->numSocios = count($this->socios);
+        echo "Incluido socio " . $numero . "<br>";
     }
 
-    public function Dvd ($titulo, $precio, $idiomas, $pantalla)
+
+    public function listarProductos()
     {
-
+        echo "Listado de los " . count($this->productos) . " productos disponibles: ";
+        foreach ($this->productos as $key => $producto) {
+            echo ($key + 1) . ".- ";
+            if ($producto instanceof Juego) {
+                echo "Juego para " . $producto->consola . ": ";
+            } else if ($producto instanceof Dvd) {
+                echo "DVD: ";
+            }
+            echo $producto->getTitulo() . "<br>";
+        }
     }
-
-    public function incluirJuego($titulo, $precio, $consola , $minJ, $maxJ)
+    public function listarSocios()
     {
-        
+        echo "Listado de " . count($this->socios) . " socios del videoclub:<br>";
+        foreach ($this->socios as $key => $cliente) {
+            echo ($key + 1) . ".- Cliente " . ($key + 1) . ": " . $cliente->nombre . "<br>";
+        }
     }
-=======
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
 
 
-
-
-
-
-    
-    
-    
-    public function incluirSocio($nombre, $maxAlquieresConcurrentes = 3){
-
-    }    
-    public function listarProductos(){
-
-    }    
-    public function listarSocios(){
-
-    }    
-    public function alquilarSocioProducto($numeroCliente, $numeroSoporte){
-
-    }
->>>>>>> a1bd3e93da292c6c663b620b48d8fa7df462c99f
+    public function alquilarSocioProducto($numeroCliente, $numeroSoporte) {}
 }
